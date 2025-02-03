f3cdab9716535f3021259a937d13c9a0d1983967
kumkumsahu123 <kumkum.sahu@techdome.net.in>
2025-02-03 23:13:21 +0530

    chnage border color

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -11,13 +11,13 @@ export default function Login() {
     }
   return (
     <div className='flex justify-center items-center h-screen bg-gray-900'>
-        <div className='border-2 border-red-900 px-18 py-30 bg-gray-800'>
+        <div className='border-2 border-green-900 px-18 py-30 bg-gray-800'>
             <form onSubmit={(e)=>handleSubmit(e)} className='flex flex-col gap-4' >
                 <input type="email" 
                 required placeholder='enter your email'
                 value={email}
                 onChange={(e)=>setemail(e.target.value)}
-                className='border-2 bg-transparent placeholder:text-gray-600  border-red-900 rounded-full px-4 py-2 text-white'
+                className='border-2 bg-transparent placeholder:text-gray-600  border-green-900 rounded-full px-4 py-2 text-white'
                 />
                 <input type="password"
                     placeholder='enter your password'
@@ -27,14 +27,14 @@ export default function Login() {
                     value={password}
                      className={`border-2 bg-transparent placeholder:text-gray-600  rounded-full 
                      text-white px-4 py-2 $
-                   border-red-900`}
+                   border-green-900`}
                 />
                 <div className='flex flex-row text-white'>
                     <input type="checkbox" />
                     <p>Remember Me</p>
                     <a href="" className='pl-4 text-red-900'>Forget password</a>
                 </div>
-                <button className='bg-red-900 text-white rounded-full mt-6 py-2'>Log In</button>
+                <button className='bg-green-900 text-white rounded-full mt-6 py-2'>Log In</button>
             </form>
         </div>
     </div>
