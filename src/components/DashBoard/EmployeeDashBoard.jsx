2cb462d45e9938f1722b1386fe98ef892a9d4f3f
kumkumsahu123 <kumkum.sahu@techdome.net.in>
2025-02-05 16:18:50 +0530

    add taskList in employeeDashBoard

--- src/components/DashBoard/EmployeeDashBoard.jsx
+++ src/components/DashBoard/EmployeeDashBoard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../others/header'
 import Task from '../others/Task'
+import TaskList from '../../TaskList/TaskList'
 export default function EmployeeDashBoard() {
   return (
     <div className='bg-gray-800 h-screen p-10 ' >
         <Header/>
         <Task/>
+        <TaskList/>
     </div>
   )
 }
